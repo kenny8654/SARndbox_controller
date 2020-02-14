422907af551309b1f4fe06121bd4d832479fb970
YiChen <kenny8654@gmail.com>
2020-02-14 11:50:36 +0800

    (feat): Add dimmer

--- app.js
+++ app.js
@@ -13,132 +13,134 @@ app.use('/public', express.static(__dirname + '/public'));
 app.use('/semantic', express.static(__dirname + '/semantic'));
 app.use('/node_modules/jquery/dist', express.static(__dirname + '/node_modules/jquery/dist'));
 app.listen(8080, function () {
-	console.log('app is running on port 8080!');}
+	console.log('app is running on port 8080!');
+}
 );
 
 app.get('/', function (req, res) {
-	res.sendFile('./index.html',{ root: __dirname });
+	res.sendFile('./index.html', { root: __dirname });
 });
 
-app.get('/getHeight',function(req,res){
-  var data = fs.readFileSync(url,'utf8');  
-  console.log(data)
+app.get('/getHeight', function (req, res) {
+	var data = fs.readFileSync(url, 'utf8');
+	console.log(data)
 	return res.end(data)
 });
 
-app.post('/modifyHeight',function(req,res){
+app.post('/modifyHeight', function (req, res) {
 	console.log('modify height');
-	console.log('info : '+req.body.info)
-	fs.writeFile(url, req.body.info , function (err) {
-    if(err)
-        console.log(err);
-    else{	
-        console.log('Write operation complete.');
-				//runcmd()
-				turnOn()
+	console.log('info : ' + req.body.info)
+	fs.writeFile(url, req.body.info, function (err) {
+		if (err)
+			console.log(err);
+		else {
+			console.log('Write operation complete.');
+			//runcmd()
+			turnOn()
 		}
 	});
 
 	return res.sendStatus(200)
 });
 
-app.get('/turnOn',function(req,res){
+app.get('/turnOn', function (req, res) {
 	console.log('on')
 	turnOn()
 	return res.sendStatus(200)
 });
 
-app.get('/turnOff',function(req,res){
+app.get('/turnOff', function (req, res) {
 	console.log('off')
-  cmd('killall',['SARndbox'])
-  cmd('killall',['-9','SARndbox'])
+	cmd('killall', ['SARndbox'])
+	cmd('killall', ['-9', 'SARndbox'])
 });
 
 
-app.get('/shutDown',function(req,res){
+app.get('/shutDown', function (req, res) {
 	console.log('off')
-  cmd('shutdown',['-h','now'])
+	cmd('shutdown', ['-h', 'now'])
 });
 
 
-app.get('/reboot',function(req,res){
-	console.log('systemctl',['reboot'])
-  cmd('reboot',[''])
+app.get('/reboot', function (req, res) {
+	console.log('systemctl', ['reboot'])
+	cmd('reboot', [''])
 });
 
-app.get('/getModeName',function(req,res){	
-  var modeNameUrl = "/home/user/src/SARndbox-2.3/etc/SARndbox-2.3/ColorMapMode/map.txt"
-	var data = fs.readFileSync(modeNameUrl,'utf8');  
-  console.log(data)
+app.get('/getModeName', function (req, res) {
+	var modeNameUrl = "/home/user/src/SARndbox-2.3/etc/SARndbox-2.3/ColorMapMode/map.txt"
+	var data = fs.readFileSync(modeNameUrl, 'utf8');
+	console.log(data)
 	return res.end(data)
 });
 
-app.post('/modifyMode',function(req,res){
-  console.log("modifyMode : " + req.body.mode)
-  var modeUrl = "/home/user/src/SARndbox-2.3/etc/SARndbox-2.3/ColorMapMode/HeightColorMap"+req.body.mode+".cpt"
+app.post('/modifyMode', function (req, res) {
+	console.log("modifyMode : " + req.body.mode)
+	var modeUrl = "/home/user/src/SARndbox-2.3/etc/SARndbox-2.3/ColorMapMode/HeightColorMap" + req.body.mode + ".cpt"
 	var mode = "/home/user/src/SARndbox-2.3/etc/SARndbox-2.3/HeightColorMap.cpt"
-	cmd('cp',[modeUrl,mode]).then(turnOn())
-  //turnOn();
+	cmd('cp', [modeUrl, mode]).then(turnOn())
+	//turnOn();
 	return res.sendStatus(200)
 });
 
-async function turnOn(){	
-  //await cmd('killall',['SARndbox'])
+async function turnOn() {
+	//await cmd('killall',['SARndbox'])
 	//await cmd('killall',['KinectUtil'])
-  await cmd('killall',['-9','-w','SARndbox'])
-  //await cmd('killall',['-9','SARndbox'])
-  //await cmd('killall',['SARndbox'])
+	await cmd('killall', ['-9', '-w', 'SARndbox'])
+	//await cmd('killall',['-9','SARndbox'])
+	//await cmd('killall',['SARndbox'])
 	//await cmd('KinectUtil',['reset','all'])
 	//await cmd('../SARndbox',['-uhm'])
-  await setTimeout(function(){cmd('killall',['SARndbox']); }, 500);
-  await setTimeout(function(){cmd('KinectUtil',['reset','all']); }, 800);
-  await setTimeout(function(){cmd('KinectUtil',['reset','all']); }, 1000);
-  await setTimeout(function(){cmd('../SARndbox',['-uhm']); }, 1500);
-	await setTimeout(function(){robot.keyTap("f12"); }, 2000);
+	await setTimeout(function () { cmd('killall', ['SARndbox']); }, 300);
+	await setTimeout(function () { cmd('killall', ['../SARndbox']); }, 500);
+	await setTimeout(function () { cmd('KinectUtil', ['reset', 'all']); }, 800);
+	await setTimeout(function () { cmd('KinectUtil', ['reset', 'all']); }, 1300);
+	await setTimeout(function () { cmd('../SARndbox', ['-uhm']); }, 2000);
+	await setTimeout(function () { robot.keyTap("f12"); }, 4000);
 
 }
 
-async function runcmd(){
-  //await cmd('killall',['SARndbox'])
+async function runcmd() {
+	//await cmd('killall',['SARndbox'])
 	//await cmd('killall',['KinectUtil'])
-	await cmd('killall',['-9','-w','SARndbox'])
-  //await cmd('killall',['-9','SARndbox'])	
-  //await cmd('killall',['SARndbox'])
-	await cmd('KinectUtil',['reset','all'])
-  await cmd('../SARndbox',['-uhm'])	
-	await setTimeout(function(){robot.keyTap("f12"); }, 2000);
+	await cmd('killall', ['-9', '-w', 'SARndbox'])
+	//await cmd('killall',['-9','SARndbox'])	
+	//await cmd('killall',['SARndbox'])
+	await cmd('KinectUtil', ['reset', 'all'])
+	await cmd('../SARndbox', ['-uhm'])
+	await setTimeout(function () { robot.keyTap("f12"); }, 2000);
 }
 
-async function cmd(c,p){
-  console.log('cmd runnung : '+ c + p)	
+async function cmd(c, p) {
+	console.log('cmd runnung : ' + c + p)
 	var ls_var = spawn(c, p);
-	await ls_var.stdout.on('data', function(data){
-  	console.log('stdout: ' + data);
+	await ls_var.stdout.on('data', function (data) {
+		console.log('stdout: ' + data);
 		return 0
 	});
 
 	ls_var.stderr.on('data', function (data) {
-    console.log('stderr: ' + data);
-    return 0
+		console.log('stderr: ' + data);
+		return 0
 	});
 
 	// 監聽 error 事件：
-	ls_var.on('error', function(code){
-  	console.log('error with code ' + code);
+	ls_var.on('error', function (code) {
+		console.log('error with code ' + code);
 	});
 
 	// 監聽 close 事件：
-	ls_var.on('close', function(code){
-  	console.log('closed with code ' + code);
+	ls_var.on('close', function (code) {
+		console.log('closed with code ' + code);
 
 	});
 
 	// 監聽 exit 事件：
-	ls_var.on('exit', function(code){
-  	console.log('exited with code ' + code);
+	ls_var.on('exit', function (code) {
+		console.log('exited with code ' + code);
 	});
 }
 
-function main(){ 
+function main() {
 	//turnOn();
 }
--- public/index.js
+++ public/index.js
@@ -1,160 +1,175 @@
-var info , height ,modeList
-async function higher(){
-  console.log('higher')
+var info, height, modeList
+async function higher() {
+	console.log('higher')
+	document.getElementById("dimmer").className = "ui active dimmer";
 	await getHeight()
 	await modifyHeight(1)
-  await getHeight()
+	await getHeight()
+	setTimeout(function () { document.getElementById("dimmer").className = "ui disabled dimmer" }, 4000);
 }
 
-async function lower(){
-	console.log('lower')	
+async function lower() {
+	document.getElementById("dimmer").className = "ui active dimmer";
+	console.log('lower')
 	await getHeight()
 	await modifyHeight(-1)
-  await getHeight()
+	await getHeight()
+	setTimeout(function () { document.getElementById("dimmer").className = "ui disabled dimmer" }, 4000);
 }
 
-function on(){
-  getHeight();
-  $.ajax({
-    url: "/turnOn",
-    type: "GET",
+function on() {
+	document.getElementById("dimmer").className = "ui active dimmer";
+	getHeight();
+	$.ajax({
+		url: "/turnOn",
+		type: "GET",
 		error: function () {
-    	console.log("Error in ajax")
+			console.log("Error in ajax")
 		}
-	});	
+	});
+	setTimeout(function () { document.getElementById("dimmer").className = "ui disabled dimmer" }, 4000);
 }
 
-function off(){
-  $.ajax({
-    url: "/turnOff",
-    type: "GET",
+function off() {
+	document.getElementById("dimmer").className = "ui active dimmer";
+	$.ajax({
+		url: "/turnOff",
+		type: "GET",
 		error: function (xhr, status, error) {
-    	console.log("Error in ajax")
+			console.log("Error in ajax")
 			console.log(error)
 		}
-	});	
+	});
+	setTimeout(function () { document.getElementById("dimmer").className = "ui disabled dimmer" }, 2000);
 }
 
-function shutdown(){	
-  $.ajax({
-    url: "/shutDown",
-    type: "GET",
+
+function shutdown() {
+	document.getElementById("dimmer").className = "ui active dimmer";
+	$.ajax({
+		url: "/shutDown",
+		type: "GET",
 		error: function (xhr, status, error) {
-    	console.log("Error in ajax")
+			console.log("Error in ajax")
 			console.log(error)
 		}
-	});	
+	});
+	setTimeout(function () { document.getElementById("dimmer").className = "ui disabled dimmer" }, 2000);
 }
 
-function reboot(){
-  $.ajax({
-    url: "/reboot",
-    type: "GET",
+function reboot() {
+	document.getElementById("dimmer").className = "ui active dimmer";
+	$.ajax({
+		url: "/reboot",
+		type: "GET",
 		error: function (xhr, status, error) {
-    	console.log("Error in ajax")
+			console.log("Error in ajax")
 			console.log(error)
 		}
-	});	
+	});
+	setTimeout(function () { document.getElementById("dimmer").className = "ui disabled dimmer" }, 2000);
 }
 
-function mode1(){
-  modifyMode(1);
+function mode1() {
+	modifyMode(1);
 }
 
-function mode2(){
-  modifyMode(2);
+function mode2() {
+	modifyMode(2);
 }
 
-function mode3(){
-  modifyMode(3);
+function mode3() {
+	modifyMode(3);
 }
 
-function mode4(){
-  modifyMode(4);
+function mode4() {
+	modifyMode(4);
 }
 
-function mode5(){
-  modifyMode(5);
+function mode5() {
+	modifyMode(5);
 }
 
-$( document ).ready(function() {
+$(document).ready(function () {
 	getHeight();
 	getModeName();
 });
 
-function modifyHeight(num){	
-	tmp_height = parseInt(height,10) + num
+function modifyHeight(num) {
+	tmp_height = parseInt(height, 10) + num
 	info = info.split(',')
 	tmp = info[3].split("(")
 	tmp[0] = tmp_height.toString() + '\n'
 	tmp = tmp.join('(')
 	info[3] = tmp
 	info = info.join(',')
-  console.log('after' + info)
-  $.ajax({
-    url: "/modifyHeight",
-    type: "POST",
-		data:{
-			info:info
+	console.log('after' + info)
+	$.ajax({
+		url: "/modifyHeight",
+		type: "POST",
+		data: {
+			info: info
 		},
-    success: function (txt) {
-			console.log(txt);	
-      getHeight()
-    },
-    error: function (xhr, status, error) {
-    	console.log("Error in ajax")
+		success: function (txt) {
+			console.log(txt);
+			getHeight()
+		},
+		error: function (xhr, status, error) {
+			console.log("Error in ajax")
 			console.log(error)
 		}
-	});	
+	});
 }
 
-function getHeight(){	
-  $.ajax({
-    url: "/getHeight",
-    type: "GET",
-    success: function (txt) {
-		info = txt
-		height = txt.split(",")[3].split("(")[0]
-    document.getElementById("height_info").innerHTML = "目前高度 : " + height
-		console.log(height)
+function getHeight() {
+	$.ajax({
+		url: "/getHeight",
+		type: "GET",
+		success: function (txt) {
+			info = txt
+			height = txt.split(",")[3].split("(")[0]
+			document.getElementById("height_info").innerHTML = "目前高度 : " + height
+			console.log(height)
 		},
 		error: function () {
-    	console.log("Error in ajax")
+			console.log("Error in ajax")
 		}
-	});	
+	});
 }
 
-function getModeName(){	
-  $.ajax({
-    url: "/getModeName",
-    type: "GET",
-    success: function (txt) {
+function getModeName() {
+	$.ajax({
+		url: "/getModeName",
+		type: "GET",
+		success: function (txt) {
 			modeList = txt.split(",")
-			for(var i = 1; i < 6; i++){
-    		document.getElementById("mode"+i.toString()).innerHTML = modeList[i-1]
+			for (var i = 1; i < 6; i++) {
+				document.getElementById("mode" + i.toString()).innerHTML = modeList[i - 1]
 			}
 		},
 		error: function () {
-    	console.log("Error in ajax")
+			console.log("Error in ajax")
 		}
-	});	
+	});
 }
 
-function modifyMode(mode){
-  console.log('modifyMode : '+mode)
-  $.ajax({
-    url: "/modifyMode",
-    type: "POST",
-		data:{
-			mode:mode
+function modifyMode(mode) {
+	document.getElementById("dimmer").className = "ui active dimmer";
+	console.log('modifyMode : ' + mode)
+	$.ajax({
+		url: "/modifyMode",
+		type: "POST",
+		data: {
+			mode: mode
 		},
-    success: function (txt) {
-			console.log(txt);	
-      document.getElementById("mode_info").innerHTML = "目前模式 : " + modeList[mode-1]
-    },
-    error: function (xhr, status, error) {
-    	console.log("Error in ajax")
+		success: function (txt) {
+			console.log(txt);
+			document.getElementById("mode_info").innerHTML = "目前模式 : " + modeList[mode - 1]
+		},
+		error: function (xhr, status, error) {
+			console.log("Error in ajax")
 			console.log(error)
 		}
-	});	
+	});
+	setTimeout(function () { document.getElementById("dimmer").className = "ui disabled dimmer" }, 4000);
 }
